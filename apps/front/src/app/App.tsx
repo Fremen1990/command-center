5de54388a957061c2026eaaece7b65324d39549c
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-03 03:37:39 +0100

    Update format:write

--- apps/front/src/app/App.tsx
+++ apps/front/src/app/App.tsx
@@ -1,6 +1,6 @@
-import NxWelcome from './nx-welcome';
+import NxWelcome from './nx-welcome'
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom'
 
 export function App() {
   return (
@@ -46,7 +46,7 @@ export function App() {
       </Routes>
       {/* END: routes */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
