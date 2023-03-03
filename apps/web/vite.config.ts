64c66499cd9e79aac933505b06cf32b58ab8aa6e
Tomasz Stanisz <fremen1990@gmail.com>
2023-03-04 00:09:10 +0100

    App initialized

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/front',
+  cacheDir: '../../node_modules/.vite/web',
 
   plugins: [
     react(),
